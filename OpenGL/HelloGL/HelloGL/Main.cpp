9b9f09e87a38566a9b2ff7fb8bf0d6b50e204454
Joel Smith <91889280+PSPMain@users.noreply.github.com>
2022-02-01 09:47:19 +0000

    initial push/creation of openGL folder

---
+++ OpenGL/HelloGL/HelloGL/Main.cpp
@@ -0,0 +1,11 @@
+#include "HelloGL.h"
+#pragma once
+#include <Windows.h>
+#include <gl/GL.h>
+#include <gl/GLU.h>
+#include <GL/>freeglut.h>
+int main(int argc, char* argv[])
+{
+	HelloGL* game = new HelloGL();
+	return 0;
+}
